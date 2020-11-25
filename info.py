d0e258ade3af29e2d13abbe129d61f4833745f2e
NuntCorp <69343659+NuntCorp@users.noreply.github.com>
2020-11-25 22:53:07 +0800

    Update info.py

--- info.py
+++ info.py
@@ -6,6 +6,12 @@ info = {
             "description": "Login Background",
             "source": "https://raw.githubusercontent.com/NuntCorp/Nunt-OS/master/background.gif",
             "size": "823 KB"
+         },
+        {
+            "name": "main.py",
+            "description": "Operating System",
+            "source": "https://raw.githubusercontent.com/NuntCorp/Nunt-OS/master/main.py",
+            "size": "17 KB"
          }
      ]
 }
