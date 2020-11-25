099fe23fa42a29b2c9c624c59bafe6ddcc247a09
NuntCorp <69343659+NuntCorp@users.noreply.github.com>
2020-11-25 22:16:22 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,11 @@
+
+info = {
+    "files": [
+        {
+            "name": "background.gif",
+            "description": "Login Background",
+            "source": "https://raw.githubusercontent.com/NuntCorp/Nunt-OS/master/background.gif",
+            "size": "823 KB"
+         }
+     ]
+}
