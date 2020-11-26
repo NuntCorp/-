cd5e10f303fb78ceb7b0b6122ae4c2511e8f97af
NuntCorp <69343659+NuntCorp@users.noreply.github.com>
2020-11-26 21:31:39 +0800

    Update info.py

--- info.py
+++ info.py
@@ -8,10 +8,16 @@ info = {
             "size": "823 KB"
          },
         {
+            "name": "logo.gif",
+            "description": "App Icon",
+            "source": "https://github.com/NuntCorp/Nunt-OS/blob/master/logo.gif?raw=true",
+            "size": "7.16 KB"
+         },
+        {
             "name": "main.py",
             "description": "Operating System",
             "source": "https://raw.githubusercontent.com/NuntCorp/Nunt-OS/master/main.py",
-            "size": "17 KB"
+            "size": "17.1 KB"
          }
      ]
 }
